754bb825861083df5c562158eb0c792a473e4f9b
unknown <mishav111mv@gmail.com>
2025-08-09 23:52:16 +0300

    одав папку 15 ласи

--- 15 Класи/Task2.py
+++ 15 Класи/Task2.py
@@ -1,59 +1,24 @@
-class TVController:
-    def __init__(self, channels):
-        self.channels = channels
-        self.current_index = 0  #
+﻿class Dog:
+    age_factor = 7
 
-    def first_channel(self):
-        self.current_index = 0
-        return self.channels[self.current_index]
+    def __init__(self):
+        self.dog_age = self.get_valid_age()
 
-    def last_channel(self):
-        self.current_index = len(self.channels) - 1
-        return self.channels[self.current_index]
+    def get_valid_age(self):
 
-    def turn_channel(self, n):
-        if 1 <= n <= len(self.channels):
-            self.current_index = n - 1
-            return self.channels[self.current_index]
-        else:
-            return "Канал не існує"
+        while True:
+            try:
+                age = int(input("Введіть вік собаки у роках: "))
+                if age <= 0:
+                    print(" Вік має бути більший за 0. Спробуйте ще раз.")
+                else:
+                    return age
+            except ValueError:
+                print("  Введіть ціле число, а не букви чи символи.")
 
-    def next_channel(self):
-        if self.current_index + 1 >= len(self.channels):
-            self.current_index = 0  # якщо останній — на перший
-        else:
-            self.current_index += 1
-        return self.channels[self.current_index]
+    def human_age(self):
 
-    def previous_channel(self):
-        if self.current_index == 0:
-            self.current_index = len(self.channels) - 1  # якщо перший — на останній
-        else:
-            self.current_index -= 1
-        return self.channels[self.current_index]
+        return self.dog_age * Dog.age_factor
 
-    def current_channel(self):
-        return self.channels[self.current_index]
-
-    def exists(self, query):
-        if isinstance(query, int):
-            if 1 <= query <= len(self.channels):
-                return "Так"
-        elif isinstance(query, str):
-            if query in self.channels:
-                return "Так"
-        return "Ні"
-
-КАНАЛИ = ["BBC", "Discovery", "TV1000"]
-controller = TVController(КАНАЛИ)
-
-print(controller.first_channel())      # ➜ BBC
-print(controller.last_channel())       # ➜ TV1000
-print(controller.turn_channel(2))      # ➜ Discovery
-print(controller.next_channel())       # ➜ TV1000
-print(controller.next_channel())       # ➜ BBC
-print(controller.previous_channel())   # ➜ TV1000
-print(controller.current_channel())    # ➜ TV1000
-print(controller.exists(4))            # ➜ Ні
-print(controller.exists("BBC"))        # ➜ Так
-print(controller.exists("CNN"))        # ➜ Ні
+my_dog = Dog()
+print(f" Вік собаки у людських роках: {my_dog.human_age()}")
