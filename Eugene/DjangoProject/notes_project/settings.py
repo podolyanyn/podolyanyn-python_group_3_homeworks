03cd37a748fc0af8efd864e675531aa0acef9b88
Yevhenii <egarbut@gmail.com>
2025-10-16 15:51:57 +0300

    Update lesson_38

--- Eugene/DjangoProject/notes_project/settings.py
+++ Eugene/DjangoProject/notes_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+aw(11$77+)-_=5_moo71_gv0(jwrhioq_23&zwifu@_6y35c!'
+SECRET_KEY = 'django-insecure-sjy*bp)i1wjila0=5%(fqwcz#wz4g5fsjqy@66=y(l63onq@iu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes',
 ]
 
 MIDDLEWARE = [
