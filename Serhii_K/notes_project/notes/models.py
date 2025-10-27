a84b66ee59df32e33c8602fbd62ce08c6776ca49
Serhii Koliada <papakati1973@gmail.com>
2025-10-27 21:12:01 +0200

    Lesson 42

--- Serhii_K/notes_project/notes/models.py
+++ Serhii_K/notes_project/notes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    cat_title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.cat_title
+
+class Note(models.Model):
+    note_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    note_title = models.CharField(max_length=200)
+    note_text = models.TextField()
+    note_reminder = models.DateTimeField("date reminder")
+    note_created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.note_title
--- Serhii_K/notes_project/notes/views.py
+++ Serhii_K/notes_project/notes/views.py
@@ -1,4 +1,94 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.template import loader
+from datetime import datetime, timedelta
 
-def my_func(request):
-    return HttpResponse("Hello from Notes app.")
+from django.db.models import F
+from django.utils import timezone
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+
+from .models import Note, Category
+from .forms import Add_note_form, Edit_note_form
+
+
+# def index(request):
+#     notes_list = Note.objects.all().order_by('-note_created_at')
+#     return render(request, 'notes/index.html', {'notes_list': notes_list})
+
+class IndexView(generic.ListView):
+    template_name = "notes/index.html"
+    context_object_name = "notes_list"
+
+    def get_queryset(self):
+        return Note.objects.all().order_by('-note_created_at')
+        # return Note.objects.filter(note_reminder__gte=F('note_created_at'))
+
+
+# def note_detail(request, note_id):
+#     note = get_object_or_404(Note, pk=note_id)
+#     return render(request, 'notes/note_detail.html', {'note': note})
+
+class DetailView(generic.DetailView):
+    model = Note
+    template_name = "notes/note_detail.html"
+
+
+def add_note(request):
+    if request.method == "POST":
+        form = Add_note_form(request.POST)
+        if form.is_valid():
+            category_name = form.cleaned_data["note_category"]
+            note_title = form.cleaned_data["note_title"]
+            note_text = form.cleaned_data["note_text"]
+            note_reminder = form.cleaned_data["note_reminder"]
+            category, created = Category.objects.get_or_create(cat_title=category_name)
+            new_note = Note(
+                note_category=category,
+                note_title=note_title,
+                note_text=note_text,
+                note_reminder=note_reminder,
+            )
+            new_note.save()
+            return redirect("notes:index")
+    else:
+        form = Add_note_form()
+
+    return render(request, "notes/add_note.html", {"form": form})
+
+
+def edit_note(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    if request.method == "POST":
+        form = Edit_note_form(request.POST)
+        if form.is_valid():
+            note.note_title = form.cleaned_data["note_title"]
+            note.note_text = form.cleaned_data["note_text"]
+            note.note_reminder = form.cleaned_data["note_reminder"]
+            # note.note_category = form.cleaned_data["note_category"]
+            category_name = form.cleaned_data["note_category"]
+            category, created = Category.objects.get_or_create(cat_title=category_name)
+            note.note_category = category
+
+            note.save()
+            return redirect("notes:note_detail", pk=note.id)
+    else:
+        # Початкові дані для форми
+        form = Edit_note_form(initial={
+            "note_title": note.note_title,
+            "note_text": note.note_text,
+            "note_category": note.note_category,
+            "note_reminder": note.note_reminder.strftime("%Y-%m-%d %H:%M"),
+        })
+
+    return render(request, "notes/edit_note.html", {"form": form, "note": note})
+
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+
+    if request.method == "POST":
+        note.delete()
+        return redirect("notes:index")
+
+    return redirect("notes:index")
